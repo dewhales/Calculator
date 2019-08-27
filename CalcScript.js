2b9bbf92067e7e43ad20213501e919706c58f32f
hardaywharlexy@gmail.com <hardaywharlexy@gmail.com>
2019-08-27 18:54:20 +0100

    adding background generator and calculator for measuration(circle)

--- CalcScript.js
+++ CalcScript.js
@@ -1,3 +1,25 @@
+/*Color picker for background generator*/
+let blockQuote = document.querySelector('blockquote');
+let color1 = document.querySelector('.color1');
+let color2 = document.querySelector('.color2');
+let body = document.querySelector('body');
+
+const SETGRADIENT = () => {
+	body.style.background = `linear-gradient(${color1.value}, ${color2.value})`;
+	blockQuote.textContent = body.style.background;
+}
+
+color1.addEventListener('input', SETGRADIENT);
+color2.addEventListener('input', SETGRADIENT);
+
+
+
+
+
+
+
+
+
 function Addition () {
 	let first = prompt ("Enter First Number");
 	let second = prompt ("Enter Second Number");
@@ -80,3 +102,59 @@ function launchCalculator() {
 
 let button = document.querySelector('.animated');
 button.addEventListener('click', launchCalculator);
+
+
+
+
+
+
+const PI = document.querySelector('#input1').value;
+let answerButton = document.querySelectorAll('button')[4];
+
+/*Calculate Area formular(PI.r2)*/
+const findValueOfArea = () => {
+	let radius = document.querySelector('#input2').value;
+	let val = Number(PI) * (Number(radius **2));
+	let area = document.querySelector('#input3').setAttribute('value', val);
+}
+
+/*Find Radius of the Circle Using the value of Area*/
+const findValueOfRadiusA = () => {
+	let area = document.querySelector('#input3').value;
+	let val = Math.sqrt(Number(area) / Number(PI));
+	let radius = document.querySelector('#input2').setAttribute('value', val);
+}
+
+/*Find Circumference of the Circle using formular(2.PI.r)*/
+const findValueOfCircumference = () => {
+	let radius = document.querySelector('#input2').value;
+	let area = document.querySelector('#input3').value;
+	let val = 2 * (Number(PI) * Number(radius));
+	let circumference = document.querySelector('#input4').setAttribute('value', val);
+}
+
+/*Find Radius of the Circle Using the value of Circumference*/
+const findValueOfRadiusC = () => {
+	let circumference = document.querySelector('#input4').value;
+	let val = Number(circumference) / Number(2 * Number(PI));
+	let radius = document.querySelector('#input2').setAttribute('value', val)
+}
+
+/*Find the missing variable in the given Circle ON CLICK.*/
+const finalAnserOnClick = () => {
+	let radius = document.querySelector('#input2').value;
+	let area = document.querySelector('#input3').value;
+	let circumference = document.querySelector('#input4').value;
+	if (Number(radius) > 0) {;
+		findValueOfArea();
+		findValueOfCircumference();
+	} else if (Number(area) > 0) {
+		findValueOfRadiusA();
+		findValueOfCircumference();
+	} else if (Number(circumference) > 0) {
+		findValueOfRadiusC();
+		findValueOfArea();
+	}
+}
+
+answerButton.addEventListener('click', finalAnserOnClick);
